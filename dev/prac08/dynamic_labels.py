4b534404a1b2c1ca70bf1bcf6e3d48156782682f
localboss <chiragverma0002@gmail.com>
2023-04-23 17:58:01 +1000

    prac_08 completed

---
+++ dev/prac08/dynamic_labels.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+from kivy.uix.label import Label
+
+
+class DynamicLabelsApp(App):
+    message = StringProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.names = {"Mukshit", "Chirag", "Jasmine"}
+
+    def build(self):
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        for name in self.names:
+            temp_label = Label(text=name)
+            self.root.ids.main.add_widget(temp_label)
+        return self.root
+
+
+DynamicLabelsApp().run()
