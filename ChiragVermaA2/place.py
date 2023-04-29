e619779a5ed0a68003983f72e8b44bee12a266b1
localboss <chiragverma0002@gmail.com>
2023-04-29 17:31:25 +1000

    done with the place.py file. added __init__, __str__ and other methods as per requirement with docstrings.

--- ChiragVermaA2/place.py
+++ ChiragVermaA2/place.py
@@ -1,9 +1,29 @@
-"""..."""
-
-
-# Create your Place class in this file
+"""Place class with mark visited/unvisited and check importance methods."""
 
 
 class Place:
-    """..."""
-    pass
+    """Represent information about a place."""
+
+    def __init__(self, name, country, priority, visited_status=False):
+        """Construct a Place from the given values."""
+        self.name = name
+        self.country = country
+        self.priority = priority
+        self.visited_status = visited_status
+
+    def __str__(self):
+        """ Returns a string representation of place object."""
+        return f"{self.name} in {self.country}, priority {self.priority}" + (
+            "(visited)" if self.visited_status else "")
+
+    def mark_visited(self):
+        """Mark the place as visited."""
+        self.visited_status = True
+
+    def mark_unvisited(self):
+        """Mark the place as unvisited."""
+        self.visited_status = False
+
+    def is_important(self):
+        """Return True if a place is important i.e. priority less than or equal to 2 else False."""
+        return self.priority <= 2
