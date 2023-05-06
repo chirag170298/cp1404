73bfa680909d916fc3c851b59d113dfdb0add799
localboss <chiragverma0002@gmail.com>
2023-05-06 23:24:34 +1000

    Spinner part is done. Added self.create_button in sort method and remove from build function so that the list gets update as per the sort key

--- ChiragVermaA2/main.py
+++ ChiragVermaA2/main.py
@@ -9,15 +9,19 @@ from kivy.lang import Builder
 from kivy import Config
 from kivy.uix.button import Button
 from kivy.properties import StringProperty
+from kivy.properties import ListProperty
 from placecollection import PlaceCollection
 
 # constants
 KV_FILE = "app.kv"
 FILENAME = "places.csv"
+SORT_KEY_TO_SORT_VALUE = {'Visited': 'is_visited', 'Name': 'name', 'Country': 'country', 'Priority': 'priority'}
 
 
 class TravelTrackerApp(App):
     """..."""
+    current_sort_key = StringProperty()
+    sort_keys = ListProperty()
     bottom_status_text = StringProperty()
 
     def __init__(self, **kwargs):
@@ -30,8 +34,17 @@ class TravelTrackerApp(App):
         self.title = "TravelTracker"
         self.root = Builder.load_file(KV_FILE)
         self.bottom_status_text = "Welcome to Travel Tracker 2.0"
-        self.create_buttons()
+        self.sort_keys = SORT_KEY_TO_SORT_VALUE.keys()
+        print(self.sort_keys)
+        self.current_sort_key = self.sort_keys[0]
         return self.root
+
+    def change_sort_key(self, sort_key):
+        print(self.place_collection)
+        self.place_collection.sort(SORT_KEY_TO_SORT_VALUE[sort_key])
+        print(self.place_collection)
+        self.root.ids.entries_box.clear_widgets()
+        self.create_buttons()
 
     def create_buttons(self):
         for place in self.place_collection:
